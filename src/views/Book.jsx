f9f5c56f2baa3bac48c773de49ceb274a1b724bc
joshua360x <jkw5499@psu.edu>
2022-05-03 14:57:42 -0400

    added select options to differentiate between old + new testaments

--- src/views/Book.jsx
+++ src/views/Book.jsx
@@ -1,46 +1,106 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Book() {
+  const history = useHistory();
+  const location = useLocation();
+  const [isLoading, setIsLoading] = useState(true);
+  const [oldTest, setOldTest] = useState(false);
+  const [newTest, setNewTest] = useState(false);
+  const [test, setTest] = useState([]);
+  const [value, setValue] = useState('all');
+  const [bibleBooks, setBibleBooks] = useState([]);
 
-const [isLoading, setIsLoading] = useState(true)
-const [bibleBooks, setBibleBooks] = useState([])
+  const updatedTestHandler = async (e) => {
+    setValue(e.target.value)
+    console.log('value :>> ', value);
+    if (value === 'all') {
+      history.push('/');
+      setOldTest(false)
+      setNewTest(false)
+    }
+    if (value === 'new') {
+      setOldTest(false)
+      setNewTest(true)
+      setTest(bibleBooks.slice(40, 66));
+      console.log('test :>> ', test);
+      history.push('/?select=new');
+    }
+    if (value === 'old') {
+      setOldTest(true)
+      setNewTest(false)
+      setTest(bibleBooks.slice(0, 39));
+      console.log('test :>> ', test);
+      history.push('/?select=old');
 
-useEffect(() => {
-  const getBooksOfBible = async () => {
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Host': 'multilingual-bible.p.rapidapi.com',
-        'X-RapidAPI-Key': '6619b0d402mshbef2e4dc706792fp1e3876jsn655f3e2729c8'
-      }
-    };
-    
-    
-    
-    const books = await fetch('https://multilingual-bible.p.rapidapi.com/kingjames/bible/english/allbooknames', options)
-    const json = await books.json()
-    console.log('json', json)
-    setBibleBooks(json)
-    setIsLoading(false)
-  }
-  getBooksOfBible();
-}, [])
+    }
+  };
+
+  useEffect(() => {
+    const getBooksOfBible = async () => {
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Host': 'multilingual-bible.p.rapidapi.com',
+          'X-RapidAPI-Key': '6619b0d402mshbef2e4dc706792fp1e3876jsn655f3e2729c8'
+        }
+      };
+      const books = await fetch('https://multilingual-bible.p.rapidapi.com/kingjames/bible/english/allbooknames', options)
+      const json = await books.json()
+      console.log('json', json)
+      // setOldTest(json.slice(0,38))
+      // setNewTest(json.slice(39,66))
+      setBibleBooks(json)
+      setIsLoading(false)
+    }
+    getBooksOfBible();
+  }, []);
 
   return (
     <>
-      {isLoading ? <h3>Bible Books are Loading...</h3> :
-      <div>
+      {isLoading ? (
+        <h3>Bible Books are Loading...</h3>
+      ) : (
+        <div>
+          <h2>Books of the Bible</h2>
+          <label htmlFor="testament"></label>
+          <select value={value} onChange={updatedTestHandler} id="testament">
+            <option value="all">All</option>
+            <option value="old">Old Testament</option>
+            <option value="new">New Testament</option>
+          </select>
 
-      <h2>Books of the Bible</h2>
-      { bibleBooks.map((bibleBook, i) => (
-        <Link to={`/bookID/${i}`} key={i}>
-        
-      <p>{bibleBook}</p>
-        </Link>
-        )) }
+          {!oldTest && !newTest ? (
+            <div>
+              {bibleBooks.map((bibleBook, i) => (
+                <Link to={`/bookID/${i}`} key={i}>
+                  <p>{bibleBook}</p>
+                </Link>
+              ))}
+            </div>
+          ) : !oldTest && newTest ? (
+            <div>
+              {test.map((bibleBook, i) => (
+                <Link to={`/bookID/${i}`} key={i}>
+                  <p>{bibleBook}</p>
+                </Link>
+              ))}
+            </div>
+          ) : oldTest && !newTest ? (
+            <div>
+              {test.map((bibleBook, i) => (
+                <Link to={`/bookID/${i}`} key={i}>
+                  <p>{bibleBook}</p>
+                </Link>
+              ))}
+            </div>
+          ) : (
+            ''
+          )}
         </div>
-       }
+      )}
     </>
-  )
+  );
 }
--- src/views/Detail.jsx
+++ src/views/Detail.jsx
@@ -6,28 +6,28 @@ const { id } = useParams()
 const [item, setItem] = useState('')
 const [isLoading, setIsLoading] = useState(true)
 useEffect(() => {
-  async function fetchID() {
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Host': 'multilingual-bible.p.rapidapi.com',
-        'X-RapidAPI-Key': '6619b0d402mshbef2e4dc706792fp1e3876jsn655f3e2729c8'
-      }
-    };
+  // async function fetchID() {
+  //   const options = {
+  //     method: 'GET',
+  //     headers: {
+  //       'X-RapidAPI-Host': 'multilingual-bible.p.rapidapi.com',
+  //       'X-RapidAPI-Key': '6619b0d402mshbef2e4dc706792fp1e3876jsn655f3e2729c8'
+  //     }
+  //   };
     
-    const books = await fetch('https://multilingual-bible.p.rapidapi.com/kingjames/bible/english/allbooknames', options)
-    const json = await books.json()
-    console.log('json', json)
-    // setBibleBooks(json)
-    setItem(json[id])
-    setIsLoading(false)
-  }
-  fetchID()
+  //   const books = await fetch('https://multilingual-bible.p.rapidapi.com/kingjames/bible/english/allbooknames', options)
+  //   const json = await books.json()
+  //   console.log('json', json)
+  //   // setBibleBooks(json)
+  //   setItem(json[id])
+  //   setIsLoading(false)
+  // }
+  // fetchID()
 }, [])
 
   return (
     <div>
-      { isLoading ? <h4>Loading Details...</h4> :
+      { isLoading ? <h4>Loading Book...</h4> :
 <h4>Hey, {item} here, my Book Number is {Number(id) + 1}</h4>
       }
     </div>
