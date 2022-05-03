4e199ede668c651f2e3be258d198defbe05f2e44
joshua360x <jkw5499@psu.edu>
2022-05-02 20:16:18 -0400

    intial setup + routes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./views/Footer";
+import Header from "./views/Header";
+import Main from "./views/Main";
+
+export default function App() {
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
---
+++ src/views/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Switch } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+
+export default function Main() {
+  return (
+    <main>
+      <Switch>
+        
+      </Switch>
+    </main>
+  )
+}
