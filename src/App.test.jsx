eaea82df4b0df2f08d0cd136c260903960783833
joshua360x <jkw5499@psu.edu>
2022-05-05 14:31:58 -0400

    added initialentry test + componenet test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,23 +1,40 @@
-import { screen, render, waitFor, waitForElementToBeRemoved } from "@testing-library/react"
-
-import { MemoryRouter } from "react-router-dom"
-import App from "./App"
-import Header from "./views/Header"
-
+import {
+  screen,
+  render,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import App from './App';
+// import Header from './views/Header';
 
 test('renders APP', async () => {
-  render( <MemoryRouter initialEntries={["/"]}>
-  <Header />
-  </MemoryRouter>
-  )
+  render(
+    <MemoryRouter initialEntries={['/']} intialIndex={0}>
+      <App />
+    </MemoryRouter>
+  );
 
   // const find = await findByText()
-  screen.getByText(/welcome/i)
+  // await screen.findByText(/welcome!/i)
   // await screen.findByText(/welcome/i)
 
   //  await waitFor(() => {
 
-  //   // screen.findByText(/loading/i)
+  await screen.findByText(/loading/i);
   //   expect(findByText(/books/i)).toBeInTheDocument()
   // })
-})
+});
+
+test('renders bible book detail', async () => {
+  render(
+    <MemoryRouter initialEntries={['/bookID/0']} initialIndex={0}>
+      <App />
+    </MemoryRouter>
+  );
+  // await waitForElementToBeRemoved(screen.getByText(/loading book.../i), {
+  //   timeout: 3000
+  // });
+  await waitFor(() => {
+    screen.getByText(/Genesis/i)}, { timeout: 3000 });
+});
